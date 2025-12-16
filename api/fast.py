ecd91a286e0e18cc48aa33c59385a7e6928e5a72
Daniel Farkas <pro.daniel.farkas@gmail.com>
2025-12-16 15:28:54 +0100

    Working and deployed API

--- api/fast.py
+++ api/fast.py
@@ -4,9 +4,14 @@ import io
 from fastapi import FastAPI
 from tensorflow import keras
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 #Our Stuff
 from hypopredict.chunker import hello
 import api.utils as utils
+
+
+class PredictRequest(BaseModel):
+    url: str
 
 app = FastAPI()
 
@@ -33,7 +38,7 @@ def read_root():
 
 
 @app.post("/predict_from_url")
-def predict_from_url(X_test_url: dict):
+def predict_from_url(request : PredictRequest):
     """
     Takes a link from Google Drive where we stored a .npy file (X_test)
     Loads the .npy file into a numpy array
@@ -44,7 +49,38 @@ def predict_from_url(X_test_url: dict):
         X_test shape, prediction results
     """
 
-    url = X_test_url['url']
+    url = request.url
+    #Extract the file ID from the Google Drive URL
+    file_id = url.split('/')[-2]
+    file = utils.drive_download_bytes(file_id)
+    X_test = np.load(io.BytesIO(file))
+
+
+    # Run prediction
+    model = app.state.model
+    print("\n", model is not None ,"\n")
+
+    predictions = app.state.model.predict(X_test)
+
+    return {
+            "predictions": predictions.tolist(),
+            "X_test_shape": X_test.shape
+            }
+
+
+@app.post("/predict_hybrid_from_url")
+def predict_hybrid_from_url(request : PredictRequest):
+    """
+    Takes a link from Google Drive where we stored a .npy file (X_test)
+    Loads the .npy file into a numpy array
+    Runs prediction using the loaded local model
+    Args:
+        X_test_url: dict - dictionary with key 'url' containing the Google Drive link to the .npy file
+    Returns:
+        X_test shape, prediction results
+    """
+
+    url = request.url
     #Extract the file ID from the Google Drive URL
     file_id = url.split('/')[-2]
     file = utils.drive_download_bytes(file_id)
