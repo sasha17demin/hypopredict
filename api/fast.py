8e6906614ce5558348c467ae04db8340bcd5e61c
Daniel Farkas <pro.daniel.farkas@gmail.com>
2025-12-18 17:43:44 +0100

    last API changes

--- api/fast.py
+++ api/fast.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from tensorflow import keras
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import gdown
 #Our Stuff
 from hypopredict.chunker import hello
 import api.utils as utils
@@ -22,7 +23,7 @@ app = FastAPI()
 cnn_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '/home/danielfarkas/code/sasha17demin/hypopredict/hypopredict/daniel_model/checkpoints/baseline/lstmcnn_baseline.keras')
 app.state.model_cnn = keras.models.load_model(cnn_path, custom_objects= {"loss": Lstmcnnmodel.focal_loss(alpha=0.75, gamma=2)})
 
-models_XSK_path = '/home/danielfarkas/code/sasha17demin/hypopredict/fusion_model_dict_0_288.pkl'
+models_XSK_path = '/home/danielfarkas/code/sasha17demin/hypopredict/api/files/fusion2_1_model_trained_dict_0_267.pkl'
 with open(models_XSK_path, 'rb') as f:
     app.state.models_dict = pickle.load(f)
 
@@ -85,15 +86,62 @@ def predict_from_url(request : PredictRequest):
             }
 
 
+@app.get("/predict_fusion_local_83")
+def predict_fusion_local_83():
+
+    with open('/home/danielfarkas/code/sasha17demin/hypopredict/api/files/demo_day_83_seq_demo.pkl', 'rb') as f:
+        X_test_cnn = pickle.load(f)
+
+    with open('/home/danielfarkas/code/sasha17demin/hypopredict/hypopredict/daniel_model/results/cnn_indexed_preds_83.pkl', 'rb') as f:
+        index_83 = pickle.load(f)
+
+    with open('/home/danielfarkas/code/sasha17demin/hypopredict/api/files/FINAL_83_DEMO_TIME_ml_prepped_cv_splits_20251218_152009.pkl', 'rb') as f:
+        split_prepped_test = pickle.load(f)['splits_prepped']
+
+    # persist the index
+    split_prepped_test[0][0].index = split_prepped_test[0][1].index
+    # stack X_trains from other splits
+    X_test = split_prepped_test[0][0].dropna(inplace=False)
+
+    # Run predictions
+    X_test_fusion = pd.DataFrame()
+    for name, model in app.state.models_dict.items():
+       if name == 'fusion':
+           pass
+       else:
+           X_test_fusion[name + '_prob'] = model.predict_proba(X_test)[:, 1]
+
+    predictions_cnn = app.state.model_cnn.predict(X_test_cnn)
+    y_pred_cnn_indexed = pd.Series(predictions_cnn.flatten(), index=index_83.index)
+    y_pred_cnn_indexed = y_pred_cnn_indexed.groupby(y_pred_cnn_indexed.index).median()
+
+    y_probs_test_fusion = app.state.models_dict['fusion'].predict_proba(X_test_fusion)[:, 1]
+    y_pred_indexed = pd.Series(y_probs_test_fusion, index=X_test.index)
+    y_pred_indexed = y_pred_indexed.groupby(y_pred_indexed.index).median()
+
+
+    return {
+            "pred_fusion" : pd.Series(y_pred_indexed).rolling(5).mean().dropna(),
+            "pred_cnn" : pd.Series(y_pred_cnn_indexed).rolling(5).mean().dropna()
+            }
+
+
 @app.get("/predict_fusion_local_64")
 def predict_fusion_local_64():
 
-    with open('/home/danielfarkas/code/sasha17demin/hypopredict/notebooks/demo_day_64_seq_demo.pkl', 'rb') as f:
+    with open('/home/danielfarkas/code/sasha17demin/hypopredict/api/files/demo_day_64_seq_demo.pkl', 'rb') as f:
         X_test_cnn = pickle.load(f)
 
-    with open('/home/danielfarkas/code/sasha17demin/hypopredict/64_ml_prepped_split_20251217_183629.pkl', 'rb') as f:
-        X_test_models = pickle.load(f)['splits_prepped'][0][0]
+    with open('/home/danielfarkas/code/sasha17demin/hypopredict/hypopredict/daniel_model/results/cnn_indexed_preds_64.pkl', 'rb') as f:
+        index_64 = pickle.load(f)
 
+    with open('/home/danielfarkas/code/sasha17demin/hypopredict/api/files/FINAL_64_DEMO_TIME_ml_prepped_cv_splits_20251218_154916.pkl', 'rb') as f:
+        split_prepped_test = pickle.load(f)['splits_prepped']
+
+    # persist the index
+    split_prepped_test[0][0].index = split_prepped_test[0][1].index
+    # stack X_trains from other splits
+    X_test = split_prepped_test[0][0].dropna(inplace=False)
 
     # Run predictions
     X_test_fusion = pd.DataFrame()
@@ -101,66 +149,82 @@ def predict_fusion_local_64():
        if name == 'fusion':
            pass
        else:
-           X_test_fusion[name + '_prob'] = model.predict_proba(X_test_models)[:, 1]
+           X_test_fusion[name + '_prob'] = model.predict_proba(X_test)[:, 1]
 
     predictions_cnn = app.state.model_cnn.predict(X_test_cnn)
+    y_pred_cnn_indexed = pd.Series(predictions_cnn.flatten(), index=index_64.index)
+    y_pred_cnn_indexed = y_pred_cnn_indexed.groupby(y_pred_cnn_indexed.index).median()
 
     y_probs_test_fusion = app.state.models_dict['fusion'].predict_proba(X_test_fusion)[:, 1]
+    y_pred_indexed = pd.Series(y_probs_test_fusion, index=X_test.index)
+    y_pred_indexed = y_pred_indexed.groupby(y_pred_indexed.index).median()
 
 
     return {
-            "predictions_cnn": predictions_cnn.tolist(),
-            "predictions_fusion": y_probs_test_fusion.tolist(),
-            "predictions_xgb": X_test_fusion['xgb_prob'].tolist(),
-            "predictions_svm": X_test_fusion['svm_prob'].tolist(),
-            "predictions_knn": X_test_fusion['knn_prob'].tolist()
+            "pred_fusion" : pd.Series(y_pred_indexed).rolling(5).mean().dropna(),
+            "pred_cnn" : pd.Series(y_pred_cnn_indexed).rolling(5).mean().dropna()
             }
 
 
-@app.post("/predict_fusion_url")
-def predict_hybrid_url(request1 : PredictRequest, request2 : PredictRequest):
 
-    with open('/home/danielfarkas/code/sasha17demin/hypopredict/notebooks/demo_day_64_seq_demo.pkl', 'rb') as f:
-        X_test_cnn = pickle.load(f)
+# Optimal solution where the users upload their data but it's a bit complicated now
+# @app.post("/predict_fusion_url")
+# def predict_hybrid_url(request1 : PredictRequest, request2 : PredictRequest):
 
-    with open('/home/danielfarkas/code/sasha17demin/hypopredict/64_ml_prepped_split_20251217_183629.pkl', 'rb') as f:
-        splits_prepped = pickle.load(f)['splits_prepped']
+#     with open('/home/danielfarkas/code/sasha17demin/hypopredict/notebooks/demo_day_64_seq_demo.pkl', 'rb') as f:
+#         X_test_cnn = pickle.load(f)
 
-    X_demo = splits_prepped[0][0]
+#     with open('/home/danielfarkas/code/sasha17demin/hypopredict/64_ml_prepped_split_20251217_183629.pkl', 'rb') as f:
+#         splits_prepped = pickle.load(f)['splits_prepped'][0][0]
 
-    print('Hi')
-    print(X_demo)
-
-    url_cnn = request1.url
-    #Extract the file ID from the Google Drive URL
-    cnn_file_id = url_cnn.split('/')[-2]
-    cnn_file = utils.drive_download_bytes(cnn_file_id)
-    X_test_cnn = pickle.load(io.BytesIO(cnn_file))
-
-    fusion_url = request2.url
-    #Extract the file ID from the Google Drive URL
-    fusion_file_id = fusion_url.split('/')[-2]
-    fusion_file = utils.drive_download_bytes(fusion_file_id)
-    X_test_models = pickle.load(io.BytesIO(fusion_file))
+#     #X_demo = splits_prepped[0][0]
 
 
-    # Run predictions
-    X_test_fusion = pd.DataFrame()
-    for name, model in app.state.models_dict.items():
-       if name == 'fusion':
-           pass
-       else:
-           X_test_fusion[name + '_prob'] = model.predict_proba(X_test_models)[:, 1]
+#     # url_cnn = request1.url
+#     # cnn_file_id = url_cnn.split('/')[-2]
+#     # gdown.download(
+#     # id=cnn_file_id,
+#     # output='cnn_file.pkl',
+#     # quiet=False
+#     # )
 
-    predictions_cnn = app.state.model_cnn.predict(X_test_cnn)
+#     # with open('cnn_file.pkl', 'rb') as f:
+#     #     X_test_cnn = pickle.load(f)
 
-    y_probs_test_fusion = app.state.models_dict['fusion'].predict_proba(X_test_fusion)[:, 1]
+
+#     #X_test_cnn = pickle.load(io.BytesIO(cnn_file))
+
+#     fusion_url = request2.url
+#     #Extract the file ID from the Google Drive URL
+#     fusion_file_id = fusion_url.split('/')[-2]
+#     gdown.download(
+#     id=fusion_file_id,
+#     output='fusion_file.pkl',
+#     quiet=False
+#     )
+
+#     with open('fusion_file.pkl', 'rb') as f:
+#         fusion_data_params = pickle.load(f)
+
+#     X_test_models = fusion_data_params['splits_prepped'][0][0]
 
 
-    return {
-            "predictions_cnn": predictions_cnn.tolist(),
-            "predictions_fusion": y_probs_test_fusion.tolist(),
-            "predictions_xgb": X_test_fusion['xgb_prob'].tolist(),
-            "predictions_svm": X_test_fusion['svm_prob'].tolist(),
-            "predictions_knn": X_test_fusion['knn_prob'].tolist()
-            }
+#     # Run predictions
+#     X_test_fusion = pd.DataFrame()
+#     for name, model in app.state.models_dict.items():
+#        if name == 'fusion':
+#            pass
+#        else:
+#            X_test_fusion[name + '_prob'] = model.predict_proba(X_test_models)[:, 1]
+
+#     #predictions_cnn = app.state.model_cnn.predict(X_test_cnn)
+
+#     y_probs_test_fusion = app.state.models_dict['fusion'].predict_proba(X_test_fusion)[:, 1]
+
+
+#     return {
+#             "predictions_fusion": y_probs_test_fusion.tolist(),
+#             "predictions_xgb": X_test_fusion['xgb_prob'].tolist(),
+#             "predictions_svm": X_test_fusion['svm_prob'].tolist(),
+#             "predictions_knn": X_test_fusion['knn_prob'].tolist()
+#             }
--- api/utils.py
+++ api/utils.py
@@ -1,29 +1,90 @@
+import re
+import html
 import requests
+from urllib.parse import urlparse, parse_qs
+from io import BytesIO
 
 def drive_download_bytes(file_id: str) -> bytes:
-    """
-    Downloads a file from Google Drive given its file ID and returns its content as bytes.
-    Args:
-        file_id (str): The unique identifier of the file on Google Drive.
-    Returns:
-        bytes: The content of the downloaded file.
-    """
-
     session = requests.Session()
     base = "https://drive.google.com/uc?export=download"
+    headers = {"User-Agent": "Mozilla/5.0"}
 
-    r = session.get(base, params={"id": file_id}, stream=True)
+    # 1) First request (often returns HTML virus warning)
+    r = session.get(base, params={"id": file_id}, headers=headers, allow_redirects=True)
     r.raise_for_status()
 
-    # Grab confirmation token if present
-    token = None
-    for k, v in r.cookies.items():
-        if k.startswith("download_warning"):
-            token = v
-            break
+    text = r.text or ""
 
-    if token:
-        r = session.get(base, params={"id": file_id, "confirm": token}, stream=True)
-        r.raise_for_status()
+    # If we got HTML, try to extract confirm token or direct download URL
+    if text.lstrip().startswith("<"):
 
-    return r.content
+        # A) Look for a direct download URL embedded in scripts (downloadUrl)
+        m = re.search(r'"downloadUrl"\s*:\s*"([^"]+)"', text)
+        if m:
+            download_url = html.unescape(m.group(1)).replace("\\u003d", "=").replace("\\u0026", "&").replace("\\/", "/")
+            rr = session.get(download_url, headers=headers, stream=True)
+            rr.raise_for_status()
+            return _read_bytes_or_fail(rr)
+
+        # B) Look for href containing confirm=...
+        m = re.search(r'href="([^"]+)"', text)
+        if m:
+            # There can be multiple hrefs; scan all
+            for href in re.findall(r'href="([^"]+)"', text):
+                if "confirm=" in href and ("uc?" in href or "download" in href):
+                    href = html.unescape(href).replace("&amp;", "&")
+                    # Make absolute if needed
+                    if href.startswith("/"):
+                        href = "https://drive.google.com" + href
+                    qs = parse_qs(urlparse(href).query)
+                    if "confirm" in qs:
+                        token = qs["confirm"][0]
+                        rr = session.get(base, params={"id": file_id, "confirm": token}, headers=headers, stream=True)
+                        rr.raise_for_status()
+                        return _read_bytes_or_fail(rr)
+
+        # C) Look for hidden input: name="confirm" value="..."
+        m = re.search(r'name="confirm"\s+value="([^"]+)"', text)
+        if m:
+            token = m.group(1)
+            rr = session.get(base, params={"id": file_id, "confirm": token}, headers=headers, stream=True)
+            rr.raise_for_status()
+            return _read_bytes_or_fail(rr)
+
+        # D) Fallback: cookie-based token (your original approach)
+        token = None
+        for k, v in r.cookies.items():
+            if k.startswith("download_warning"):
+                token = v
+                break
+        if token:
+            rr = session.get(base, params={"id": file_id, "confirm": token}, headers=headers, stream=True)
+            rr.raise_for_status()
+            return _read_bytes_or_fail(rr)
+
+        # Nothing worked -> show a helpful snippet
+        snippet = text[:500]
+        raise RuntimeError(
+            "Got an HTML interstitial from Google Drive (virus scan warning), "
+            "but couldn't extract a confirm token or download URL.\n"
+            f"HTML starts with:\n{snippet}"
+        )
+
+    # If not HTML, it’s already the file
+    rr = session.get(base, params={"id": file_id}, headers=headers, stream=True)
+    rr.raise_for_status()
+    return _read_bytes_or_fail(rr)
+
+
+def _read_bytes_or_fail(response: requests.Response) -> bytes:
+    buf = BytesIO()
+    for chunk in response.iter_content(chunk_size=65536):
+        if chunk:
+            buf.write(chunk)
+    data = buf.getvalue()
+
+    if data.lstrip().startswith(b"<"):
+        snippet = data[:300].decode("utf-8", errors="replace")
+        raise RuntimeError("Still received HTML, not the file. Preview:\n" + snippet)
+
+    return data
